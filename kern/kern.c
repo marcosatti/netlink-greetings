26ba1f359a2fd22fba6e56f4eca330518e84d97d
Marco Satti <marcosatti@gmail.com>
2020-07-28 12:36:47 +0800

    Netlink example work

---
+++ kern/kern.c
@@ -0,0 +1,88 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/netlink.h>
+#include <net/sock.h>
+#include <linux/string.h>
+
+#define NETLINK_TEST_PROTOCOL 31
+
+static struct sock *nl_sock = NULL;
+
+static void netlink_recv_msg_fn(struct sk_buff *skb_in);
+
+static struct netlink_kernel_cfg cfg = {
+        .input = netlink_recv_msg_fn
+};
+
+static void nlmsg_dump(struct nlmsghdr *nlh) {
+        printk(KERN_INFO "Netlink message dump: length = %u, type = %hu, flags = %hu, sequence ID = %u, unique (process) ID = %u", 
+                nlh->nlmsg_len,
+                nlh->nlmsg_type,
+                nlh->nlmsg_flags,
+                nlh->nlmsg_seq,
+                nlh->nlmsg_pid);
+}
+
+static int __init netlink_greetings_init(void) {
+        nl_sock = netlink_kernel_create(&init_net, NETLINK_TEST_PROTOCOL, &cfg);
+        if (!nl_sock) {
+                printk(KERN_ERR "Failed to create netlink socket");
+                return -ENOMEM;
+        }
+                
+        printk(KERN_INFO "Netlink greetings init ok");
+        return 0;
+}
+
+static void __exit netlink_greetings_exit(void) {
+        if (nl_sock) {
+                netlink_kernel_release(nl_sock);
+                nl_sock = NULL;                
+        }
+
+        printk(KERN_INFO "Netlink greetings exit ok");
+}
+
+static void netlink_recv_msg_fn(struct sk_buff *skb_in) {
+        struct nlmsghdr *nlh_recv, *nlh_reply;
+        __u32 pid;
+        char *us_data;
+        char reply[256];
+        struct sk_buff *skb_out;
+        int us_data_len, reply_result;
+
+        printk(KERN_INFO "%s() invoked", __FUNCTION__);
+
+        nlh_recv = nlmsg_hdr(skb_in);
+
+        nlmsg_dump(nlh_recv);
+
+        pid = nlh_recv->nlmsg_pid;
+        us_data = nlmsg_data(nlh_recv);
+        us_data_len = nlmsg_len(nlh_recv);
+
+        printk(KERN_INFO "Netlink message from port %u: len = %u, message = %s", pid, us_data_len, us_data);
+
+        if (nlh_recv->nlmsg_flags & NLM_F_ACK) {
+                memset(reply, 0, sizeof(reply));
+                snprintf(reply, sizeof(reply), "Msg from port %u acknowledged", pid);
+
+                skb_out = nlmsg_new(sizeof(reply), 0);
+                nlh_reply = nlmsg_put(skb_out, 0, nlh_recv->nlmsg_seq, NLMSG_DONE, sizeof(reply), 0);
+                strncpy(nlmsg_data(nlh_reply), reply, sizeof(reply));
+
+                reply_result = nlmsg_unicast(nl_sock, skb_out, pid);
+                if (reply_result < 0) {
+                        printk(KERN_ERR "Error sending data back to user space");
+                }
+        
+                kfree_skb(skb_out);
+        }
+}
+
+module_init(netlink_greetings_init);
+module_exit(netlink_greetings_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Marco Satti <marcosatti@gmail.com>");
+MODULE_DESCRIPTION("Netlink greetings example");
---
+++ user/user.c
@@ -0,0 +1,89 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <linux/netlink.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define NETLINK_TEST_PROTOCOL 31
+
+static int send_netlink_msg_to_kernel(int sock_fd, char *msg, uint32_t msg_size, int nlmsg_type, uint16_t flags) {
+        struct sockaddr_nl dest_addr;
+        memset(&dest_addr, 0, sizeof(dest_addr));
+        dest_addr.nl_family = AF_NETLINK;
+        dest_addr.nl_pid = 0;
+
+        struct nlmsghdr *nlh = (struct nlmsghdr *)calloc(1, NLMSG_HDRLEN + NLMSG_SPACE(msg_size));
+        nlh->nlmsg_len = NLMSG_HDRLEN + NLMSG_SPACE(msg_size);
+        nlh->nlmsg_pid = getpid();
+        nlh->nlmsg_type = nlmsg_type;
+        nlh->nlmsg_seq = 0;
+        nlh->nlmsg_flags = flags;
+
+        strncpy(NLMSG_DATA(nlh), msg, msg_size);
+
+        struct iovec iov;
+        iov.iov_base = (void *)nlh;
+        iov.iov_len = nlh->nlmsg_len;
+
+        struct msghdr outermsghdr;
+        memset(&outermsghdr, 0, sizeof(struct msghdr));
+        outermsghdr.msg_name = (void *)&dest_addr;
+        outermsghdr.msg_namelen = sizeof(dest_addr);
+        outermsghdr.msg_iov = &iov;
+        outermsghdr.msg_iovlen = 1;
+
+        return sendmsg(sock_fd, &outermsghdr, 0);
+}
+
+int main(char *argv[], int argc) {
+        int sock_fd = socket(AF_NETLINK, SOCK_DGRAM, NETLINK_TEST_PROTOCOL);
+        if (sock_fd < 0) {
+                printf("Failed to create socket\n");
+                return EXIT_FAILURE;
+        }
+
+        struct sockaddr_nl src_addr;
+        memset(&src_addr, 0, sizeof(src_addr));
+        src_addr.nl_family = AF_NETLINK;
+        src_addr.nl_pid = getpid();
+
+        int bind_ret = bind(sock_fd, (const struct sockaddr*)&src_addr, sizeof(src_addr));
+        if (bind_ret < 0) {
+                printf("Failed to bind socket\n");
+                return EXIT_FAILURE;
+        }
+
+        int count = 0;
+
+        while (1) {
+                printf("Press enter to send message\n");
+                char ch = getchar();
+
+                if (ch == 'q') {
+                        printf("Quitting\n");
+                        return EXIT_SUCCESS;
+                }
+
+                if (ch == '\n') {
+                        char message[256] = { 0 };
+                        int message_size = sizeof(message);
+                        snprintf(message, message_size, "Hello world %d", count);
+                        count++;
+                        
+                        uint16_t flags = NLM_F_ACK;
+                        flags = 0;
+                        
+                        int send_ret = send_netlink_msg_to_kernel(sock_fd, message, message_size, 1, flags);
+                        if (send_ret < 0) {
+                                printf("Failed to send message\n");
+                                return EXIT_FAILURE;                        
+                        }
+                }
+        }
+
+        return EXIT_SUCCESS;
+}
